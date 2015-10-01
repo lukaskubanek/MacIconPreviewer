03d39deadb56cf24b9676ca8bc528658523eb51a
Lukas Kubanek <lukas.kubanek@me.com>
2015-10-01 13:59:09 +0200

    Added initial Xcode project

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  MacIconPreviewer
+//
+//  Created by Lukas Kubanek on 01/10/15.
+//  Copyright © 2015 Lukas Kubanek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+    }
+    
+}
---
+++ Sources/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  MacIconPreviewer
+//
+//  Created by Lukas Kubanek on 01/10/15.
+//  Copyright © 2015 Lukas Kubanek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+}
